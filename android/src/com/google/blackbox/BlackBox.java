b7007bd8f7c8bf1c1606ea398f0bf16848f2c022
philv <philv@google.com>
2009-12-23 23:08:15 +0000

    File structure created by   android create project --package com.google.blackbox --activity BlackBox --target 1 --path android
    Renamed build.properties to build.properties.template

---
+++ android/src/com/google/blackbox/BlackBox.java
@@ -0,0 +1,15 @@
+package com.google.blackbox;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BlackBox extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
