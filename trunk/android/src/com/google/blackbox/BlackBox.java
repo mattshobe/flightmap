c83062e63202c2cc4da193342febb5c8b1e4f7c6
philv@google.com <philv@google.com@a73d2fca-3060-11df-9f25-831aa717a150>
2010-01-05 04:02:11 +0000

    Work in progress. Trying to read the database from the SD card. Not working now because some android metadata is missing from the database.

--- trunk/android/src/com/google/blackbox/BlackBox.java
+++ trunk/android/src/com/google/blackbox/BlackBox.java
@@ -1,5 +1,5 @@
 /* 
- * Copyright (C) 2009 Google Inc.
+ * Copyright (C) 2010 Google Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,18 +12,43 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */package com.google.blackbox;
+ */
+package com.google.blackbox;
 
-import android.app.Activity;
+import android.app.ListActivity;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.widget.SimpleCursorAdapter;
 
-public class BlackBox extends Activity
-{
+public class BlackBox extends ListActivity {
+    private AirportDbAdapter airportReader;
+
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.airport_list);
+        airportReader = new AirportDbAdapter();
+        airportReader.open();
+        readAirportData();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        airportReader.close();
+    }
+
+    private void readAirportData() {
+        Cursor c = airportReader.fetchAllAirports();
+        startManagingCursor(c);
+
+        // Create an adapter to display the cursor using the airport_row layout.
+        String[] from = new String[] { AirportDbAdapter.ICAO_COLUMN,
+                AirportDbAdapter.NAME_COLUMN };
+        int[] to = new int[] { R.id.ident, R.id.name };
+        SimpleCursorAdapter airports = new SimpleCursorAdapter(this,
+                R.layout.airport_row, c, from, to);
+        setListAdapter(airports);
     }
 }
