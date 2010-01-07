f8109a5165337c5fa48ac912b08be8ee67f169ba
philv@google.com <philv@google.com@a73d2fca-3060-11df-9f25-831aa717a150>
2010-01-07 20:09:52 +0000

    Removed commented-out code and reformatted

--- trunk/android/src/com/google/blackbox/BlackBox.java
+++ trunk/android/src/com/google/blackbox/BlackBox.java
@@ -1,17 +1,17 @@
-/* 
+/*
  * Copyright (C) 2010 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package com.google.blackbox;
 
@@ -22,59 +22,34 @@ import android.util.Log;
 import android.widget.SimpleCursorAdapter;
 
 public class BlackBox extends ListActivity {
-    private static final String TAG = BlackBox.class.getSimpleName();
-    private AirportDbAdapter airportReader;
+  private static final String TAG = BlackBox.class.getSimpleName();
+  private AirportDbAdapter airportReader;
 
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.airport_list);
-        airportReader = new AirportDbAdapter(this);
-        airportReader.open();
-        readAirportData();
-    }
+  /** Called when the activity is first created. */
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.airport_list);
+    airportReader = new AirportDbAdapter(this);
+    airportReader.open();
+    Log.d(TAG, "About to read airport data");
+    readAirportData();
+  }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        airportReader.close();
-    }
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+    airportReader.close();
+  }
 
-    private void readAirportData() {
-        Cursor c = airportReader.fetchAllAirports();
-//        int icaoColumn = c.getColumnIndex(AirportDbAdapter.ICAO_COLUMN);
-//        int nameColumn = c.getColumnIndex(AirportDbAdapter.NAME_COLUMN);
-//        Log.d(TAG, "icao column = " + icaoColumn);
-//        Log.d(TAG, "name column = " + nameColumn);
-//        if (null == c) {
-//            Log.e(TAG, "Null cursor");
-//            return;
-//        }
-//
-//        boolean ok = c.moveToFirst();
-//        Log.d(TAG, "  moveToFirst status " + ok);
-//        if (!ok) {
-//            Log.e(TAG, "Returning early -- moveToFirst failed");
-//            return;
-//        }
-//
-//        Log.d(TAG, "Before looping");
-//        do {
-//            String icao = c.getString(icaoColumn);
-//            String name = c.getString(nameColumn);
-//            Log.d(TAG, String.format("%s: %s", icao, name));
-//        } while (c.moveToNext());
-//        Log.d(TAG, "After looping");
+  private void readAirportData() {
+    Cursor c = airportReader.fetchAllAirports();
+    startManagingCursor(c);
 
-        startManagingCursor(c);
-
-        // Create an adapter to display the cursor using the airport_row layout.
-        String[] from = new String[] { AirportDbAdapter.ICAO_COLUMN,
-                AirportDbAdapter.NAME_COLUMN };
-        int[] to = new int[] { R.id.ident, R.id.name };
-        SimpleCursorAdapter airports = new SimpleCursorAdapter(this,
-                R.layout.airport_row, c, from, to);
-        setListAdapter(airports);
-    }
+    // Create an adapter to display the cursor using the airport_row layout.
+    String[] from = new String[] {AirportDbAdapter.ICAO_COLUMN, AirportDbAdapter.NAME_COLUMN};
+    int[] to = new int[] {R.id.ident, R.id.name};
+    SimpleCursorAdapter airports = new SimpleCursorAdapter(this, R.layout.airport_row, c, from, to);
+    setListAdapter(airports);
+  }
 }
