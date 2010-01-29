4865b4605cd6c1da232fed8cb59acab8bbf5f5eb
bonnieritchey <bonnieritchey@google.com>
2010-01-29 05:40:44 +0000

    Making this a service instead of an activity and adding output to file

--- android/src/com/google/blackbox/ManageLocation.java
+++ android/src/com/google/blackbox/ManageLocation.java
@@ -1,22 +1,26 @@
 package com.google.blackbox;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import android.app.Activity;
 import android.app.Notification;
+import android.app.Service;
 import android.content.Context;
+import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.widget.Toast;
 
-public class ManageLocation extends Activity implements LocationListener {
-    /** Called when the activity is first created. */
+public class ManageLocation extends Service implements LocationListener {
   
   private LocationManager locationManager = null;
-  private Context context;
   private double myAltitude = 0;
   private double myLat = 0;
   private double myLng = 0;
@@ -24,35 +28,46 @@ public class ManageLocation extends Activity implements LocationListener {
   private float myBearing = 0;
   private Location lastLocation = null;
   protected final long MIN_DIST_DIFF = 15/3; // approximately 5 feet
-  protected final long MIN_TIME_DIFF = 5000; // milliseconds
+  protected final long MIN_TIME_DIFF = 5000; // 5 seconds
   protected List<Location> myPath = new ArrayList<Location>();
   private long time = 0; 
   private boolean isAlerting = false;
   private boolean isRecording  = false;
   private long startTime = 0;
+  private FileOutputStream fs = null;
 
-  
-  public ManageLocation(Context context){
-    this.context = context;
-  
-  /** Called when the activity is first created. */
-/*  @Override
-  public void onCreate(Bundle savedInstanceState) {
-      super.onCreate(savedInstanceState);
-     setContentView(R.layout.main); */
+    @Override
+    public void onCreate() {
+      super.onCreate();
+      setForeground(true);
+      try {
+      locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+      }
+      catch (Exception e)
+      {
+        e.printStackTrace();
+      }
+      lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+      locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME_DIFF, MIN_DIST_DIFF, this);
 
-    locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-    lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME_DIFF, MIN_DIST_DIFF, this);
-
-    Notification notification = new Notification(R.drawable.icon, null, System.currentTimeMillis());
-    isRecording = true;
-    startTime = System.currentTimeMillis();
-    isAlerting = true;
-    startLocationListener();
+      Notification notification = new Notification(R.drawable.icon, null, System.currentTimeMillis());
+      isRecording = true;
+      startTime = System.currentTimeMillis();
+      isAlerting = true;
+      
+      File appDir = new File("/sdcard/blackbox");
+      if(!appDir.exists()){
+        appDir.mkdirs();
+      }
+      try {
+        fs = new FileOutputStream(appDir.getPath() + "/myPath.txt");
+      } catch (FileNotFoundException e1) {
+        // TODO Auto-generated catch block
+        e1.printStackTrace();
+      }
     
-
-}  
+    
+    }  
   
   /*
    * Returns a Location containing Longitude, Latitude, Altitude in meters
@@ -74,10 +89,6 @@ public class ManageLocation extends Activity implements LocationListener {
   {
     // TODO write out lastLocation to file or memory
   }
-  
-    
- 
-
 
 public void startLocationListener() {
   
@@ -87,21 +98,21 @@ public void startLocationListener() {
 }
 
 @Override
-public void onResume() {
-  super.onResume();
-  isAlerting = true;
-}
-
-@Override
-public void onPause() {
-  super.onPause();
-  isAlerting = false;
-}
-
-@Override
 public void onDestroy() {
   isRecording = false;
   isAlerting = false;
+  try {
+    fs.flush();
+  } catch (IOException e) {
+    // TODO Auto-generated catch block
+    e.printStackTrace();
+  }
+  try {
+    fs.close();
+  } catch (IOException e) {
+    // TODO Auto-generated catch block
+    e.printStackTrace();
+  }
   locationManager.removeUpdates(this);
   super.onDestroy();
 }
@@ -116,15 +127,24 @@ public void onLocationChanged(Location location) {
   myLng = lastLocation.getLongitude();
   mySpeed = lastLocation.getSpeed() * 2.2369;
   time = lastLocation.getTime();
-  
   myBearing = lastLocation.getBearing();
   
   
-  String myStats = "ALT: " + myAltitude + "\nLAT: " + myLat + "\nLONG: " + 
-  myLng + "\nSPD: " + mySpeed + "\nHDING: " + myBearing + "\nTIME: " + time;
+  String myStats = "TIME: " + time + "\tALT: " + myAltitude + "\tLAT: " +
+  myLat + "\tLONG: " + myLng + "\tSPD: " + mySpeed + "\tHDING: " + 
+  myBearing + "\n";
   
   if(isAlerting)
-    Toast.makeText(ManageLocation.this, myStats, Toast.LENGTH_LONG).show();
+    Toast.makeText(this, myStats, Toast.LENGTH_LONG).show();
+  try {
+    fs.write(myStats.getBytes());
+  } catch (IOException e) {
+    // TODO Auto-generated catch block
+    e.printStackTrace();
+  }
+  
+  if((System.currentTimeMillis() - startTime) > 500000)
+    this.onDestroy();
 }
 
 @Override
@@ -143,5 +163,11 @@ public void onProviderEnabled(String provider) {
 public void onStatusChanged(String provider, int status, Bundle extras) {
   // TODO Auto-generated method stub
   
+}
+
+@Override
+public IBinder onBind(Intent intent) {
+  // TODO Auto-generated method stub
+  return null;
 }
 }
