30a0fcac8a5f5e46b5e6f8b0300050c82758bd55
philv@google.com <philv@google.com@a73d2fca-3060-11df-9f25-831aa717a150>
2010-02-09 02:31:32 +0000

    - Created new LocationService which replaces ManageLocation.   - Did not remove ManageLocation.java, but it probably can go now.   - New ILocationService aidl - Temporarily removed the code to show nearest airports because the current   implementation is too slow to keep up with location updates. - The UI now shows the current location. This is temporary to help us figure   out how to use a service to get location information.

--- trunk/android/src/com/google/blackbox/BlackBox.java
+++ trunk/android/src/com/google/blackbox/BlackBox.java
@@ -16,22 +16,42 @@
 package com.google.blackbox;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.location.Location;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.RemoteException;
 import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
-
-import com.google.blackbox.AirportDbAdapter.AirportDistance;
-import com.google.blackbox.data.LatLng;
+import android.widget.Toast;
 
 public class BlackBox extends Activity {
   private static final String TAG = BlackBox.class.getSimpleName();
+  /** Milliseconds beteween screen updates. */
+  private static final int UPDATE_RATE = 100;
+  private static final double METERS_PER_FOOT = 3.2808399;
+  private static final double METERS_PER_SEC_TO_KNOTS = 2.2369;
   private AirportDbAdapter airportReader;
+  private final LocationServiceConnection locationServiceConnection =
+      new LocationServiceConnection();
+  private boolean isRunning;
+  private long lastUpdateTime;
+  private UpdateHandler updater = new UpdateHandler();
 
-  /** Called when the activity is first created. */
+  // Debugging frequency of location updates
+  private long numLocationUpdates;
+  private long timeBetweenUpdatesTotal;
+  private long lastLocationTime;
+
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -42,27 +62,81 @@ public class BlackBox extends Activity {
   @Override
   protected void onResume() {
     super.onResume();
-    AirportDistance[] airportDistances = airportReader.getNearestAirports(
-        new LatLng(37385888, -122083748), 10);
-    if (null == airportDistances) {
-      Log.w(TAG, "No airports returned");
+    bindService(new Intent(ILocationService.class.getName()), locationServiceConnection,
+        Context.BIND_AUTO_CREATE);
+    isRunning = true;
+    update();
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+    isRunning = false;
+    unbindService(locationServiceConnection);
+  }
+
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+    airportReader.close();
+  }
+
+  /**
+   * Updates the view every {@link #UPDATE_RATE} milliseconds using
+   * {@link UpdateHandler}.
+   */
+  private void update() {
+    if (!isRunning) {
       return;
     }
+    long now = System.currentTimeMillis();
+    if (now - lastUpdateTime > UPDATE_RATE) {
+      drawUi();
+      lastUpdateTime = now;
+    }
+    updater.scheduleUpdate(UPDATE_RATE);
+  }
+
+  private void drawUi() {
     TableLayout table = new TableLayout(this);
-    table.setLayoutParams(new LinearLayout.LayoutParams(
-        LinearLayout.LayoutParams.FILL_PARENT,
+    table.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT,
         LinearLayout.LayoutParams.FILL_PARENT));
     table.setColumnStretchable(1, true);
-    for (int i = 0; i < airportDistances.length; i++) {
-      AirportDistance airportDistance = airportDistances[i];
-      TableRow row = new TableRow(this);
-      row.addView(createTextView(airportDistance.airport.icao));
-      row.addView(createTextView(airportDistance.airport.name));
-      row.addView(createTextView(String
-          .format("%.1f", airportDistance.distance)));
-      table.addView(row);
+    Location location = locationServiceConnection.getLocation();
+    if (null == location) {
+      table.addView(createLabelValueRow("Location", "Unknown"));
+      setContentView(table);
+      return;
+    }
+    table.addView(createLabelValueRow("Lat", String.format("%4.6f", location.getLatitude())));
+    table.addView(createLabelValueRow("Lng", String.format("%4.6f", location.getLongitude())));
+    table.addView(createLabelValueRow("Alt", String.format("%.0f", location.getAltitude()
+        * METERS_PER_FOOT)));
+    table.addView(createLabelValueRow("Knots", String.format("%.0f", location.getSpeed()
+        * METERS_PER_SEC_TO_KNOTS)));
+    table.addView(createLabelValueRow("Bearing", String.format("%.1f", location.getBearing())));
+    table.addView(createLabelValueRow("Accuracy", String.format("%.3f", location.getAccuracy())));
+    table.addView(createLabelValueRow("Location time", "" + location.getTime()));
+    table.addView(createLabelValueRow("Current time", "" + System.currentTimeMillis()));
+    if (lastLocationTime != location.getTime()) {
+      numLocationUpdates++;
+      if (lastLocationTime > 0) {
+        timeBetweenUpdatesTotal += location.getTime() - lastLocationTime;
+      }
+      lastLocationTime = location.getTime();
+    }
+    if (numLocationUpdates > 0) {
+      table.addView(createLabelValueRow("Avg update rate", ""
+          + Math.round((float) timeBetweenUpdatesTotal / numLocationUpdates)));
     }
     setContentView(table);
+  }
+
+  private TableRow createLabelValueRow(String label, String value) {
+    TableRow result = new TableRow(this);
+    result.addView(createTextView(label));
+    result.addView(createTextView(value));
+    return result;
   }
 
   private View createTextView(String text) {
@@ -72,9 +146,56 @@ public class BlackBox extends Activity {
     return result;
   }
 
-  @Override
-  protected void onDestroy() {
-    super.onDestroy();
-    airportReader.close();
+  /**
+   * Updates the UI using a delayed message.
+   */
+  private class UpdateHandler extends Handler {
+    private static final int UPDATE_MESSAGE = 1;
+
+    @Override
+    public void handleMessage(Message msg) {
+      update();
+    }
+
+    /**
+     * Call {@link #update} after {@code delay} milliseconds.
+     */
+    public void scheduleUpdate(long delay) {
+      removeMessages(UPDATE_MESSAGE);
+      sendMessageDelayed(obtainMessage(UPDATE_MESSAGE), delay);
+    }
+  }
+
+  /**
+   * Connects to the ILocationService.
+   */
+  private class LocationServiceConnection implements ServiceConnection {
+    private ILocationService locationService;
+
+    public Location getLocation() {
+      if (null != locationService) {
+        try {
+          return locationService.getLocation();
+        } catch (RemoteException e) {
+          Log.e(TAG, "RPC exception", e);
+        }
+      } else {
+        Log.d(TAG, "locationService is null");
+      }
+      return null;
+    }
+
+    @Override
+    public void onServiceConnected(ComponentName name, IBinder service) {
+      locationService = ILocationService.Stub.asInterface(service);
+      Toast.makeText(BlackBox.this, "Connected to ILocationService", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onServiceDisconnected(ComponentName name) {
+      Toast.makeText(BlackBox.this, "Disconnected from ILocationService", Toast.LENGTH_LONG).show();
+      locationService = null;
+    }
+
   }
 }
