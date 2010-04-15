44968685be403a56ab9d7924dad319ce8ae7d45b
bonnieritchey@google.com <bonnieritchey@google.com@a73d2fca-3060-11df-9f25-831aa717a150>
2010-04-15 21:54:28 +0000

    fixed prefs typos and added public prefs variables

--- trunk/android/src/com/google/flightmap/android/UserPrefs.java
+++ trunk/android/src/com/google/flightmap/android/UserPrefs.java
@@ -7,8 +7,19 @@ import android.preference.PreferenceManager;
 public class UserPrefs extends PreferenceActivity {
 	
     public static final String PREFS_NAME = "FlightMapPrefs";
-
-
+    
+    /* 
+     * Default Preferences Values - Needs to be synced with preferences.xml
+     */
+    public static final boolean NORTH_UP = true;
+    public static final int UPDATE_INTERVAL = 100; //milliseconds
+    public static final String DISTANCE_UNITS = "mile"; // mile, km, nm
+    public static final boolean SHOW_SEAPLANE = true;
+    public static final boolean SHOW_MILITARY = true;
+    public static final boolean SHOW_SOFT = true;
+    public static final boolean SHOW_PRIVATE = true;
+    public static final int RUNWAY_LENGTH = 2000; //in feet
+    
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
