79f1aed5cec2319bed88112df726966d7b1e35fe
bonnieritchey <bonnieritchey@google.com>
2010-04-16 21:27:18 +0000

    adding more preferences work

--- android/src/com/google/flightmap/android/UserPrefs.java
+++ android/src/com/google/flightmap/android/UserPrefs.java
@@ -1,5 +1,6 @@
 package com.google.flightmap.android;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
@@ -9,18 +10,18 @@ public class UserPrefs extends PreferenceActivity {
     public static final String PREFS_NAME = "FlightMapPrefs";
     
     /* 
-     * Default Preferences Values - Needs to be synced with preferences.xml
+     * Default Preferences Values - Matches preferences.xml
      */
-    public static final boolean NORTH_UP = true;
-    public static final int UPDATE_INTERVAL = 100; //milliseconds
-    public static final String DISTANCE_UNITS = "mile"; // mile, km, nm
-    public static final boolean SHOW_SEAPLANE = true;
-    public static final boolean SHOW_MILITARY = true;
-    public static final boolean SHOW_SOFT = true;
-    public static final boolean SHOW_PRIVATE = true;
-    public static final int RUNWAY_LENGTH = 2000; //in feet
+    public static final String NORTH_UP = "North_Up";
+    public static final String UPDATE_INTERVAL = "Update_Interval";
+    public static final String DISTANCE_UNITS = "Distance_Units";
+    public static final String SHOW_SEAPLANE = "Show_Seaplane";
+    public static final String SHOW_MILITARY = "Show_Military";
+    public static final String SHOW_SOFT = "Show_Soft";
+    public static final String SHOW_PRIVATE = "Show_Private";
+    public static final String RUNWAY_LENGTH = "Runway_Length";  
     
-	@Override
+    @Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
@@ -28,6 +29,8 @@ public class UserPrefs extends PreferenceActivity {
 		preferenceManager.setSharedPreferencesName(PREFS_NAME);
 		preferenceManager.setSharedPreferencesMode(0);
 		addPreferencesFromResource(R.xml.preferences);
+		
+		
 	}
- 
+	 
 }
