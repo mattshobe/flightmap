3358f1c3ef906fedccc3334f5d6e48d6c3edf8ee
aristidis@google.com <aristidis@google.com@a73d2fca-3060-11df-9f25-831aa717a150>
2010-07-02 07:34:10 +0000

    Added intersect() and remove() methods to LatLngRect. Creates test folder for common project. Added unittest: LatLngRectTest.

--- trunk/common/src/com/google/flightmap/common/data/LatLngRect.java
+++ trunk/common/src/com/google/flightmap/common/data/LatLngRect.java
@@ -15,6 +15,9 @@
  */
 package com.google.flightmap.common.data;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+
 /**
  * Rectangle with sides that run north-south and east-west.
  */
@@ -61,10 +64,10 @@ public class LatLngRect {
       neCorner = newPoint;
       return;
     }
-    int northLat = Math.max(neCorner.lat, newPoint.lat);
-    int southLat = Math.min(swCorner.lat, newPoint.lat);
-    int eastLng = Math.max(neCorner.lng, newPoint.lng);
-    int westLng = Math.min(swCorner.lng, newPoint.lng);
+    final int northLat = Math.max(neCorner.lat, newPoint.lat);
+    final int southLat = Math.min(swCorner.lat, newPoint.lat);
+    final int eastLng = Math.max(neCorner.lng, newPoint.lng);
+    final int westLng = Math.min(swCorner.lng, newPoint.lng);
     neCorner = new LatLng(northLat, eastLng);
     swCorner = new LatLng(southLat, westLng);
   }
@@ -81,8 +84,15 @@ public class LatLngRect {
    * @return true if point is in this area, false otherwise.
    */
   public synchronized boolean contains(final LatLng point) {
-    return point.lat >= swCorner.lat && point.lat <= neCorner.lat &&
-           point.lng >= swCorner.lng && point.lng <= neCorner.lng;
+    return contains(point.lat, point.lng);
+  }
+
+  protected synchronized boolean contains(final int latE6, final int lngE6) {
+    if (isEmpty()) {
+      return false;
+    }
+    return latE6 >= swCorner.lat && latE6 <= neCorner.lat &&
+           lngE6 >= swCorner.lng && lngE6 <= neCorner.lng;
   }
 
   /**
@@ -98,6 +108,30 @@ public class LatLngRect {
    */
   public synchronized boolean isEmpty() {
     return swCorner == null || neCorner == null;
+  }
+
+  @Override
+  public synchronized boolean equals(Object obj) {
+    if (obj == null) {
+      return false;
+    }
+
+    if (this == obj) {
+      return true;
+    }
+
+    if (!(obj instanceof LatLngRect)) {
+      return false;
+    }
+
+    final LatLngRect other = (LatLngRect)obj;
+    return (isEmpty() && other.isEmpty()) || 
+           (neCorner.equals(other.neCorner) && swCorner.equals(other.swCorner));
+  }
+
+  @Override
+  public int hashCode() {
+    return neCorner.hashCode() ^ swCorner.hashCode();
   }
 
   @Override
@@ -144,4 +178,79 @@ public class LatLngRect {
     return swCorner.lng;
   }
 
+  /**
+   * Returns intersection of {@code this} and {@code that}.
+   */
+  public synchronized LatLngRect intersect(final LatLngRect that) {
+    if (isEmpty() || that.isEmpty()) {
+      return new LatLngRect();
+    }
+
+    final int thisWest = this.getWest();
+    final int thisEast = this.getEast();
+    final int thisNorth = this.getNorth();
+    final int thisSouth = this.getSouth();
+    final int thatWest = that.getWest();
+    final int thatEast = that.getEast();
+    final int thatNorth = that.getNorth();
+    final int thatSouth = that.getSouth();
+
+    if (thisWest > thatEast || thisEast < thatWest ||
+        thisSouth > thatNorth || thisNorth < thatSouth) {
+      return new LatLngRect();
+    }
+
+    final int interWest = Math.max(thisWest, thatWest);
+    final int interEast = Math.min(thisEast, thatEast);
+    final int interSouth = Math.max(thisSouth, thatSouth);
+    final int interNorth = Math.min(thisNorth, thatNorth);
+    final LatLng interNE = new LatLng(interNorth, interEast);
+    final LatLng interSW = new LatLng(interSouth, interWest);
+    return new LatLngRect(interNE, interSW);
+  }
+
+  private static LatLngRect getRectOrNullIfEmpty(final int lat1, final int lng1,
+                                                 final int lat2, final int lng2) {
+    if (lat1 == lat2 || lng1 == lng2) {
+      return null;
+    }
+
+    final LatLng corner1 = new LatLng(lat1, lng1);
+    final LatLng corner2 = new LatLng(lat2, lng2);
+    return new LatLngRect(corner1, corner2);
+  }
+
+  /**
+   * Returns relative complement of {@code that} in {@code this}.
+   * In set notation: this \ that (or this-that)
+   */
+  public synchronized LinkedList<LatLngRect> remove(final LatLngRect that) {
+    final LinkedList<LatLngRect> complementRects = new LinkedList<LatLngRect>();
+    final LatLngRect intersection = this.intersect(that);
+    if (intersection.isEmpty()) {
+      complementRects.add(new LatLngRect(neCorner, swCorner));
+    } else {
+      final int thisNorth = getNorth();
+      final int thisSouth = getSouth();
+      final int thisWest = getWest();
+      final int thisEast = getEast();
+      final int interNorth = intersection.getNorth();
+      final int interSouth = intersection.getSouth();
+      final int interWest = intersection.getWest();
+      final int interEast = intersection.getEast();
+
+      complementRects.add(getRectOrNullIfEmpty(thisNorth, thisWest, interNorth, thisEast));
+      complementRects.add(getRectOrNullIfEmpty(interNorth, thisWest, interSouth, interWest));
+      complementRects.add(getRectOrNullIfEmpty(interNorth, interEast, interSouth, thisEast));
+      complementRects.add(getRectOrNullIfEmpty(interSouth, thisWest, thisSouth, thisEast));
+
+      for (Iterator<LatLngRect> i = complementRects.iterator(); i.hasNext(); ) {
+        final LatLngRect next = i.next();
+        if (next == null) {
+          i.remove();
+        }
+      }
+    }
+    return complementRects;
+  }
 }
