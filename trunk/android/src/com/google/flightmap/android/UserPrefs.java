07e0a8130d16abe126d93b64c4d788967c71a92d
bonnieritchey@google.com <bonnieritchey@google.com@a73d2fca-3060-11df-9f25-831aa717a150>
2010-08-26 23:10:28 +0000

    Changed menu to XML and added preference for only updating the database once per day

--- trunk/android/src/com/google/flightmap/android/UserPrefs.java
+++ trunk/android/src/com/google/flightmap/android/UserPrefs.java
@@ -18,6 +18,7 @@ package com.google.flightmap.android;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.text.format.Time;
 
 import com.google.flightmap.common.NavigationUtil.DistanceUnits;
 
@@ -38,6 +39,8 @@ public class UserPrefs {
   private static final String SHOW_HELIPORT = "Show_Heliports";
   private static final String RUNWAY_LENGTH = "Runway_Length";
   private static final String FAKE_HEADING = "Fake_Heading";
+  private static final String ALWAYS_UPDATE = "Always_Update";
+  private static final String LAST_UPDATED = "Last_Updated";
 
   // Preference values. These match values in arrays.xml
   private static final String DISTANCE_UNITS_MILES = "1";
@@ -95,6 +98,39 @@ public class UserPrefs {
   public synchronized int getMinRunwayLength() {
     String runwayString = sharedPrefs.getString(RUNWAY_LENGTH, RUNWAY_LENGTH_2000);
     return getMinRunwayLengthFromPreference(runwayString);
+  }
+
+  /**
+   * By default always update at startup.
+   */
+  public synchronized boolean getAlwaysUpdate() {
+    return sharedPrefs.getBoolean(ALWAYS_UPDATE, true);
+  }
+  
+  /**
+   * Set database update time.
+   */
+  public synchronized boolean setUpdateTime(long time) {
+	  SharedPreferences.Editor editor = sharedPrefs.edit();
+	  editor.putLong(LAST_UPDATED, time);
+	  editor.commit();
+	  return true;
+  }
+
+  /**
+   * Returns true if the last updated time was more than 24 hours ago or the
+   * preference is set to always update.
+   */
+  public synchronized boolean getNeedToUpdate() {
+	  long updateTime = sharedPrefs.getLong(LAST_UPDATED, 0);
+	  if(updateTime == 0)
+		  return true;
+	  else {
+		  Time curr = new Time();
+		  curr.setToNow();
+		  long now = curr.toMillis(true);
+		  return (now - updateTime > 86400000);
+	  }
   }
 
   /**
