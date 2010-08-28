cee274540a112b04924ba719c571c5c88723d891
philv@google.com <philv@google.com@a73d2fca-3060-11df-9f25-831aa717a150>
2010-08-28 01:26:37 +0000

    Work in progress Tapcard UI improvements - Show appropriately colored top bar w/ ICAO and name.   - May not be able to do row-spanning of ICAO as in mock.   - No drop shadow for ICAO yet. - Created UiConstants class to hold shared paints, fonts, etc. - Minor code reorg in TapcardActivity

---
+++ trunk/android/src/com/google/flightmap/android/UiConstants.java
@@ -0,0 +1,31 @@
+// Copyright 2010 Google Inc. All Rights Reserved.
+
+package com.google.flightmap.android;
+
+import android.graphics.Color;
+import android.graphics.Paint;
+
+/**
+ * Constants such as paints and fonts shared across multiple UI screens.
+ */
+class UiConstants {
+  static final Paint AIRCRAFT_PAINT = new Paint();
+  static final Paint NON_TOWERED_PAINT = new Paint();
+  static final Paint TOWERED_PAINT = new Paint();
+
+  // Static initialization.
+  static {
+    // Do not put any calls to setTextSize here. Put them in #setTextSizes().
+    TOWERED_PAINT.setAntiAlias(true);
+    TOWERED_PAINT.setARGB(0xff, 0x0, 0xcc, 0xff);
+    TOWERED_PAINT.setTextAlign(Paint.Align.CENTER);
+    NON_TOWERED_PAINT.setAntiAlias(true);
+    NON_TOWERED_PAINT.setARGB(0xff, 0xcc, 0x33, 0xcc);
+    NON_TOWERED_PAINT.setTextAlign(Paint.Align.CENTER);
+    AIRCRAFT_PAINT.setColor(Color.GREEN);
+    AIRCRAFT_PAINT.setStrokeWidth(3);
+  }
+
+  private UiConstants() {
+  }
+}
