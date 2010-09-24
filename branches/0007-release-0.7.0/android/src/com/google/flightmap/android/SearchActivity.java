ffaae33528681ac8461f3a093f073ef150fd8e95
philv@google.com <philv@google.com@a73d2fca-3060-11df-9f25-831aa717a150>
2010-09-24 15:47:28 +0000

    Create 0.7.0 release branch

---
+++ branches/0007-release-0.7.0/android/src/com/google/flightmap/android/SearchActivity.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright (C) 2010 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.flightmap.android;
+
+import android.app.ListActivity;
+/*
+ * Copyright (C) 2010 Google Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+import android.app.SearchManager;
+import com.google.flightmap.common.AviationDbAdapter;
+import com.google.flightmap.common.CachedAviationDbAdapter;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+
+/**
+ * Activity for search within MainActivity.
+ */
+public class SearchActivity extends ListActivity {
+  private static final String TAG = SearchActivity.class.getSimpleName();
+
+  private AviationDbAdapter aviationDbAdapter;
+  private UserPrefs userPrefs;
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.search);
+
+    userPrefs = new UserPrefs(getApplication());
+    try {
+      aviationDbAdapter = new CachedAviationDbAdapter(new AndroidAviationDbAdapter(userPrefs));
+      // Open database connection.
+      aviationDbAdapter.open();
+      } catch (Throwable t) {
+        Log.w(TAG, "Unable to open database", t);
+        finish();
+        }
+      handleIntent(getIntent());
+
+  }
+  
+  @Override
+  protected void onNewIntent(Intent intent) {
+    setIntent(intent);
+    handleIntent(intent);
+  }
+
+  private void handleIntent(Intent intent) {
+    if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+      String query = intent.getStringExtra(SearchManager.USER_QUERY);
+      doSearch(query);
+    }
+  }
+	
+  private void doSearch(String query) {
+     int airportId = aviationDbAdapter.getAirportIdByIcao(query);
+     if (airportId == -1 && query.length() == 3) {
+       // Try again with 'K' prepended if it's only 3 letters.
+       airportId = aviationDbAdapter.getAirportIdByIcao("K" + query);
+     }
+     if(airportId != -1) {
+       showTapcard(airportId);
+     }
+     else {
+       // Can return "like" results down the road.
+       String[] items = {query + " not found"};
+       setListAdapter(new ArrayAdapter(this,android.R.layout.simple_list_item_1, items));
+     }
+  }
+  
+  private void showTapcard(int airportId) {
+    Intent tapcardIntent = new Intent(this, TapcardActivity.class);
+    tapcardIntent.putExtra(TapcardActivity.AIRPORT_ID, airportId);
+    this.startActivity(tapcardIntent);
+  }
+}
