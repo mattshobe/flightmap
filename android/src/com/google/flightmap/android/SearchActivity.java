d1bacc08933ffdb71f644c7a89dbf3e1efaa198d
bonnieritchey <bonnieritchey@google.com>
2010-09-24 01:41:52 +0000

    Updated based on comments for r327.

--- android/src/com/google/flightmap/android/SearchActivity.java
+++ android/src/com/google/flightmap/android/SearchActivity.java
@@ -17,18 +17,33 @@
 package com.google.flightmap.android;
 
 import android.app.ListActivity;
-
+/*
+ * Copyright (C) 2010 Google Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 import android.app.SearchManager;
 import com.google.flightmap.common.AviationDbAdapter;
 import com.google.flightmap.common.CachedAviationDbAdapter;
-import com.google.flightmap.common.data.Airport;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ArrayAdapter;
-import android.widget.SimpleCursorAdapter;
 
+/**
+ * Activity for searching for airports by icao within MainActivity.
+ */
 public class SearchActivity extends ListActivity {
   private static final String TAG = SearchActivity.class.getSimpleName();
 
@@ -38,17 +53,19 @@ public class SearchActivity extends ListActivity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    // Basic layout for showing search results.
     setContentView(R.layout.search);
-    // Open database connection.
 
     userPrefs = new UserPrefs(getApplication());
     try {
       aviationDbAdapter = new CachedAviationDbAdapter(new AndroidAviationDbAdapter(userPrefs));
+      // Open database connection.
       aviationDbAdapter.open();
       } catch (Throwable t) {
         Log.w(TAG, "Unable to open database", t);
         finish();
         }
+      // Handle the intent this way to avoid multiple tapcard instances.
       handleIntent(getIntent());
 
   }
@@ -65,23 +82,26 @@ public class SearchActivity extends ListActivity {
       doSearch(query);
     }
   }
-	
+
+  /**
+   * Searches for an airport ID based on an icao as the input query.
+   */
   private void doSearch(String query) {
-     int airportId = aviationDbAdapter.getAirportIdByICAO(query);
-     if (airportId == -1 && query.length() == 3) {
-       // Try again with 'K' prepended if it's only 3 letters.
-       airportId = aviationDbAdapter.getAirportIdByICAO("K" + query);
-     }
-     if(airportId != -1) {
-       showTapcard(airportId);
-     }
-     else {
-       // Can return "like" results.
-       String[] items = {query + " not found"};
-       setListAdapter(new ArrayAdapter(this,android.R.layout.simple_list_item_1, items));
-     }
+    // 
+    int airportId = aviationDbAdapter.getAirportIdByICAO(query);
+    // -1 returned if the icao was not recognized. Since many pilots use only
+    // the last 3 letters if an icao, try prepending a 'K' and retrying.
+    if (airportId == -1 && query.length() == 3) {
+      airportId = aviationDbAdapter.getAirportIdByICAO("K" + query);
+    }
+    if(airportId != -1) {
+      showTapcard(airportId);
+    }
   }
   
+  /**
+   * Starts the Tapcard Activity using the ID of the airport from doSearch.
+   */
   private void showTapcard(int airportId) {
     Intent tapcardIntent = new Intent(this, TapcardActivity.class);
     tapcardIntent.putExtra(TapcardActivity.AIRPORT_ID, airportId);
