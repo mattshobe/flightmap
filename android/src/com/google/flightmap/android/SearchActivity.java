919a4e3329e4741bc1f421b22aba35ee9f42aaf7
bonnieritchey <bonnieritchey@google.com>
2010-10-08 04:53:09 +0000

    Addressing comments for r380. Fixed formatting and variable names.

--- android/src/com/google/flightmap/android/SearchActivity.java
+++ android/src/com/google/flightmap/android/SearchActivity.java
@@ -50,109 +50,115 @@ import android.widget.ListView;
  * Activity for search within MainActivity.
  */
 public class SearchActivity extends ListActivity {
-  private static final String TAG = SearchActivity.class.getSimpleName();
+	private static final String TAG = SearchActivity.class.getSimpleName();
 
-  private AviationDbAdapter aviationDbAdapter;
-  private UserPrefs userPrefs;
+	private AviationDbAdapter aviationDbAdapter;
+	private UserPrefs userPrefs;
 
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.search);
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.search);
 
-    userPrefs = new UserPrefs(getApplication());
-    try {
-      aviationDbAdapter = new CachedAviationDbAdapter(new AndroidAviationDbAdapter(userPrefs));
-      // Open database connection.
-      aviationDbAdapter.open();
-      } catch (Throwable t) {
-        Log.w(TAG, "Unable to open database", t);
-        finish();
-        }
-      handleIntent(getIntent());
+		userPrefs = new UserPrefs(getApplication());
+		try {
+			aviationDbAdapter = new CachedAviationDbAdapter(
+					new AndroidAviationDbAdapter(userPrefs));
+			// Open database connection.
+			aviationDbAdapter.open();
+		} catch (Throwable t) {
+			Log.w(TAG, "Unable to open database", t);
+			finish();
+		}
+		handleIntent(getIntent());
 
-  }
-  
-  /**
-   * This method will be called when an item in the list is selected and
-   * show the tapcard for the selected item.
-   */
-  @Override
-  protected void onListItemClick(ListView l, View v, int position, long id) {
-	super.onListItemClick(l, v, position, id);
-	String content = (String) getListView().getItemAtPosition(position);
-	String icao = content.substring(0, 3);
-	int airportId = aviationDbAdapter.getAirportIdByIcao(icao);
-    if (airportId == -1) {
-        // Try again with 'K' prepended if it's only 3 letters.
-        // TODO: Move this down to the list results.
-        airportId = aviationDbAdapter.getAirportIdByIcao(content.substring(0,4));
-      }
-      if(airportId != -1) {
-        showTapcard(airportId);
-      }
-  }
+	}
 
-  
-  @Override
-  protected void onNewIntent(Intent intent) {
-    setIntent(intent);
-    handleIntent(intent);
-  }
+	/**
+	 * This method will be called when an item in the list is selected and show
+	 * the tapcard for the selected item.
+	 */
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		super.onListItemClick(l, v, position, id);
+		String content = (String) getListView().getItemAtPosition(position);
+		String icao = content.substring(0, 3);
+		int airportId = aviationDbAdapter.getAirportIdByIcao(icao);
+		if (airportId == -1) {
+			// Try again with 'K' prepended if it's only 3 letters.
+			// TODO: Move this down to the list results.
+			airportId = aviationDbAdapter.getAirportIdByIcao(content.substring(
+					0, 4));
+		}
+		if (airportId != -1) {
+			showTapcard(airportId);
+		}
 
-  private void handleIntent(Intent intent) {
-    if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-      String query = intent.getStringExtra(SearchManager.USER_QUERY);
-      doSearch(query);
-    }
-  }
-	
-  private void doSearch(String query) {
-     int airportId = aviationDbAdapter.getAirportIdByIcao(query);
-     if (airportId == -1 && query.length() == 3) {
-       // Try again with 'K' prepended if it's only 3 letters.
-       // TODO: Move this down to the list results.
-       airportId = aviationDbAdapter.getAirportIdByIcao("K" + query);
-     }
-     if(airportId != -1) {
-       showTapcard(airportId);
-     }
-     else {
-       // Replaces spaces with % for the %LIKE% db search.
-       String query_like = query.replace(' ', '%');
-       query_like = '%' + query_like + '%';
-       List<Integer> airportsName = aviationDbAdapter.getAirportIdsWithNameLike(query_like);
-       List<Integer> airports = aviationDbAdapter.getAirportIdsWithCityLike(query_like);
-       Iterator<Integer> iter1 = airportsName.iterator();
-       // Remove duplicates. Iterate through airportsName and if it's not in
-       // airports List, add it.
-       while(iter1.hasNext()) {
-    	   int id = iter1.next();
-    	   if(!airports.contains(id)) {
-    		   airports.add(id);
-    	   }
-       }
-       if (airports.isEmpty()) {
-     	  String[] items = {query + " not found"};
-          setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items));
-       } else {
-         Iterator<Integer> iter2 = airports.iterator();
-         int i=0;
-      	 String[] airportList = new String[airports.size()];
-         while(iter2.hasNext()) {
-    	   int id = iter2.next();
-    	   String showName = aviationDbAdapter.getAirport(id).icao + " " + aviationDbAdapter.getAirport(id).name;
-    	   airportList[i] = showName;
-    	   i++;
-    	 }
-    	 setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, airportList));
-       }
-     }
-  }
-  
-  private void showTapcard(int airportId) {
-    Intent tapcardIntent = new Intent(this, TapcardActivity.class);
-    tapcardIntent.putExtra(TapcardActivity.AIRPORT_ID, airportId);
-    this.startActivity(tapcardIntent);
-  }
+	}
+
+	@Override
+	protected void onNewIntent(Intent intent) {
+		setIntent(intent);
+		handleIntent(intent);
+	}
+
+	private void handleIntent(Intent intent) {
+		if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+			String query = intent.getStringExtra(SearchManager.USER_QUERY);
+			doSearch(query);
+		}
+	}
+
+	private void doSearch(String query) {
+		int airportId = aviationDbAdapter.getAirportIdByIcao(query);
+		if (airportId == -1 && query.length() == 3) {
+			// Try again with 'K' prepended if it's only 3 letters.
+			// TODO: Move this down to the list results.
+			airportId = aviationDbAdapter.getAirportIdByIcao("K" + query);
+		}
+		if (airportId != -1) {
+			showTapcard(airportId);
+		} else {
+			// Replaces spaces with % for the %LIKE% db search.
+			String query_like = query.replace(' ', '%');
+			query_like = '%' + query_like + '%';
+			List<Integer> airportsName = aviationDbAdapter
+					.getAirportIdsWithNameLike(query_like);
+			List<Integer> airports = aviationDbAdapter
+					.getAirportIdsWithCityLike(query_like);
+			Iterator<Integer> nameIterator = airportsName.iterator();
+			// Merge nameMatches into the airports list. That list will have all
+			// matches by name or city.
+			while (nameIterator.hasNext()) {
+				int id = nameIterator.next();
+				if (!airports.contains(id)) {
+					airports.add(id);
+				}
+			}
+			if (airports.isEmpty()) {
+				String[] items = { query + " not found" };
+				setListAdapter(new ArrayAdapter<String>(this,
+						android.R.layout.simple_list_item_1, items));
+			} else {
+				Iterator<Integer> airportIterator = airports.iterator();
+				int i = 0;
+				String[] airportList = new String[airports.size()];
+				while (airportIterator.hasNext()) {
+					int id = airportIterator.next();
+					String showName = aviationDbAdapter.getAirport(id).icao
+							+ " " + aviationDbAdapter.getAirport(id).name;
+					airportList[i] = showName;
+					i++;
+				}
+				setListAdapter(new ArrayAdapter<String>(this,
+						android.R.layout.simple_list_item_1, airportList));
+			}
+		}
+	}
+
+	private void showTapcard(int airportId) {
+		Intent tapcardIntent = new Intent(this, TapcardActivity.class);
+		tapcardIntent.putExtra(TapcardActivity.AIRPORT_ID, airportId);
+		this.startActivity(tapcardIntent);
+	}
 }
