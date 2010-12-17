c46e56078a8e619db8e195621ab14a001759744f
aristidis@google.com <aristidis@google.com@a73d2fca-3060-11df-9f25-831aa717a150>
2010-12-17 19:41:33 +0000

    Imposed order on Comm frequencies using identifier, remarks and frequency. This groups frequencies with same identifier by remarks in tapcard.

--- trunk/common/src/com/google/flightmap/common/data/Comm.java
+++ trunk/common/src/com/google/flightmap/common/data/Comm.java
@@ -16,7 +16,7 @@
 
 package com.google.flightmap.common.data;
 
-public class Comm {
+public class Comm implements Comparable<Comm> {
   public final String identifier;
   public final String frequency;
   public final String remarks;
@@ -25,5 +25,33 @@ public class Comm {
     this.identifier = identifier;
     this.frequency = frequency;
     this.remarks = remarks;
+  }
+
+  @Override
+  public int compareTo(final Comm o) {
+    final int identifierDiff = identifier.compareTo(o.identifier);
+    if (identifierDiff != 0) {
+      return identifierDiff;
+    }
+    final int remarksDiff = remarks.compareTo(o.remarks);
+    if (remarksDiff != 0) {
+      return remarksDiff;
+    }
+    final int frequencyDiff = frequency.compareTo(o.frequency);
+    return frequencyDiff;
+  }
+
+  @Override
+  public boolean equals(final Object obj) {
+    if (obj == this) return true;
+    if (!(obj instanceof Comm)) return false;
+
+    final Comm other = (Comm) obj;
+    return compareTo(other) == 0;
+  }
+
+  @Override
+  public int hashCode() {
+    return identifier.hashCode() ^ frequency.hashCode() ^ remarks.hashCode();
   }
 }
